aaa1690c6c6a860eaab5cf219545be6d9d2170d8
mendhak <mendhak@users.noreply.github.com>
2022-01-15 18:13:19 +0000

    Play sound on button press

--- app/src/main/java/com/mendhak/sheepyhorn/MainActivity.kt
+++ app/src/main/java/com/mendhak/sheepyhorn/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.mendhak.sheepyhorn
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener, MediaPlayer.OnCompletionListener {
+
+    private lateinit var player: MediaPlayer
+    private var alreadyPlaying: Boolean = false;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        player = MediaPlayer.create(applicationContext, R.raw.sheepyhorn)
+        player.setOnCompletionListener(this)
+
+        findViewById<ImageButton>(R.id.sound_button).setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        if(!alreadyPlaying){
+            player.start();
+            alreadyPlaying = true
+        }
+    }
+
+    override fun onCompletion(mp: MediaPlayer?) {
+        alreadyPlaying = false
+    }
+
 }
